5aa7485ec9e41629849ea7be59774a466bcdc2d3
MatteoMit <matteo.mitrano@3mlinformatica.it>
2020-08-11 15:56:16 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,42 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
-
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular Reactive Forms Example';
+export class AppComponent {
+  name = "Angular Reactive Forms Example";
 
-contactForm = new FormGroup({
-  firstname: new FormControl('', [Validators.required,Validators.minLength(2)]),
-  lastname: new FormControl(),
-  email: new FormControl(),
-  gender: new FormControl(),
-  isMarried: new FormControl(),
-  country: new FormControl(),
-  address:new FormGroup({
-    city: new FormControl(),
-    street: new FormControl(),
-    pincode:new FormControl()
-  })
-})
+  contactForm = new FormGroup({
+    firstname: new FormControl("", [
+      Validators.required,
+      Validators.minLength(2)
+    ]),
+    lastname: new FormControl(),
+    email: new FormControl(),
+    gender: new FormControl(),
+    isMarried: new FormControl(),
+    country: new FormControl(),
+    address: new FormGroup({
+      city: new FormControl(),
+      street: new FormControl(),
+      pincode: new FormControl()
+    })
+  });
 
- onSubmit() {
-  console.log(this.contactForm.value);
-} 
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+}
+
+export class country {
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
